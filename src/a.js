a10e44b14aeeb1a82c5112b31740003f9879780d
Marina Zinchenko <iolsoofuolso@gmail.com>
2022-12-27 14:21:23 -0800

    install webpack and set init build

---
+++ src/a.js
@@ -0,0 +1,4 @@
+// import _ from 'lodash';
+// const arr = ['gvenya', 'marmu', 'spiral', 'rosie'];
+// export const foo = () => console.log(_.indexOf(arr, 'marmu')); // 'hello module a');
+export const foo = () => console.log('hi module a');
---
+++ src/index.js
@@ -0,0 +1,4 @@
+// import './style.css';
+import { foo } from './a.js';
+
+foo();
