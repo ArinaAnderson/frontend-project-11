8b50ed2e2e16681f03f1a426b7e3c56e1ec4f875
Marina Zinchenko <iolsoofuolso@gmail.com>
2023-04-02 22:26:24 -0700

    remove handleModalOpening.js and add its function openModal to view.js

--- src/view.js
+++ src/view.js
@@ -1,6 +1,19 @@
 import onChange from 'on-change';
-import openModal from './handleModalOpening';
 // import has from 'lodash/has.js';
+
+const openModal = (evt, state) => {
+  evt.preventDefault();
+  const btn = evt.target;
+  const postID = btn.dataset.id;
+  const { title, description, link } = state.posts.find((post) => post.id === postID);
+  const modal = document.querySelector('#modal');
+  const modalTitle = modal.querySelector('.modal-title');
+  modalTitle.textContent = title;
+  const modalBody = modal.querySelector('.modal-body');
+  modalBody.textContent = description;
+  const modalLink = modal.querySelector('.full-article');
+  modalLink.href = link;
+};
 
 const renderInput = (input, isValid) => {
   if (!isValid) {
