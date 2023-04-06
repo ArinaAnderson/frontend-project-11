08f5cfc6fcd2f569eafa614a30286509f6f528a1
Marina Zinchenko <iolsoofuolso@gmail.com>
2023-04-05 23:26:52 -0700

    replace search of unique posts by title with search by url

--- src/updateFeeds.js
+++ src/updateFeeds.js
@@ -8,43 +8,20 @@ const TIME_INTERVAL = 5000;
 const updateFeedsHandler = (state) => {
   const { feeds: allFeeds, posts: allPosts } = state;
 
-  const allTitles = allPosts.map((post) => post.title);
+  const allLinks = allPosts.map((post) => post.link);
   const promises = allFeeds.map(({ rssLink, id: feedID }) => {
     const requestedData = sendRequest(rssLink);
     return requestedData
       .then((data) => {
-        // if (data.status === 'fulfilled') {
-        // const parsedRSS = parseRSS(data.value.contents);
         const parsedRSS = parseRSS(data.contents);
         const { posts } = handlePayload(rssLink, parsedRSS, feedID, uniqueId);
         const newPosts = posts
-          .filter((post) => !allTitles.includes(post.title));
+          .filter((post) => !allLinks.includes(post.link));
         state.posts = newPosts.concat(state.posts);
-        // }
       });
-    /*
-    .then((data) => {
-        const parsedRSS = parseRSS(data.contents);
-        const { posts } = handlePayload(rssLink, parsedRSS, feedID, uniqueId);
-        const newPosts = posts
-          .filter((post) => !allTitles.includes(post.title));
-        state.posts = newPosts.concat(state.posts);
-        // return newPosts;
-      });
-    */
   });
   return Promise.allSettled(promises)
     .finally(() => setTimeout(() => updateFeedsHandler(state), TIME_INTERVAL));
-  /*
-  return Promise.all(promises)
-    .then((arrayOfNewPosts) => arrayOfNewPosts
-      .flat()
-      .filter((el) => el !== null))
-    .then((newPosts) => {
-      state.posts = newPosts.concat(state.posts);
-    })
-    .then(() => setTimeout(() => updateFeedsHandler(state), TIME_INTERVAL));
-  */
 };
 
 const startFeedsUpdate = (state) => {
