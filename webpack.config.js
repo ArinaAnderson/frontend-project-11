a8b9e5ac4326403e4cc5c4dc8030b7d48d9450f2
Marina Zinchenko <iolsoofuolso@gmail.com>
2023-05-01 21:34:51 -0700

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ import { fileURLToPath } from 'url';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 export default {
   devtool: 'inline-source-map',
@@ -50,7 +50,7 @@ export default {
   ],
   devServer: {
     static: {
-      directory: path.resolve(__dirname, 'dist'),
+      directory: path.resolve(dirname, 'dist'),
     },
     open: true,
     hot: true,
